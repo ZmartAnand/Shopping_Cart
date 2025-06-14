54eac85c57bc9ef64d83ec0c485aa4ea8b01a8f8
Anand <s.anandsiva7897@gmail.com>
2025-06-14 17:42:29 +0530

    change

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -16,23 +16,42 @@ export class ProfileComponent implements OnInit {
 
   constructor(private auth: Auth) {}
 
+  // ngOnInit(): void {
+  //   const currentUser = localStorage.getItem("currentUser");
+  //   if (currentUser) {
+  //     console.log(currentUser);
+
+  //     this.user = JSON.parse(currentUser);
+  //     this.auth.onAuthStateChanged((user: any) => {
+  //       console.log("User:", user);
+  //     });
+  //   }
+  //   const username = this.user?.username || "User";
+  //   const savedPhoto = localStorage.getItem("profilePhoto");
+
+  //   this.uploadedPhoto = savedPhoto ? savedPhoto : null;
+  // }
   ngOnInit(): void {
-    const currentUser = localStorage.getItem("currentUser");
-    if (currentUser) {
-      console.log(currentUser);
+    const currentUserString = localStorage.getItem("currentUser");
+    if (currentUserString) {
+      try {
+        this.user = JSON.parse(currentUserString);
 
-      this.user = JSON.parse(currentUser);
-      this.auth.onAuthStateChanged((user: any) => {
-        console.log("User:", user);
-      });
+        const username = this.user.displayName || this.user.username || "User";
+        console.log(this.user);
+        const userPhoto = this.user.photo || this.user.photoURL || "";
+
+        const savedPhoto = localStorage.getItem("profilePhoto");
+
+        this.uploadedPhoto = savedPhoto || userPhoto || this.defaultAvatar;
+      } catch (error) {
+        console.error("Error parsing currentUser:", error);
+      }
+    } else {
+      this.user = { username: "User" };
+      this.uploadedPhoto = this.defaultAvatar;
     }
-    const username = this.user?.username || "User";
-    const savedPhoto = localStorage.getItem("profilePhoto");
-
-    // this.avatarUrl = `https://ui-avatars.com/api/?name=${username}&background=0D8ABC&color=fff`;
-    this.uploadedPhoto = savedPhoto ? savedPhoto : null;
   }
-
   onPhotoSelected(event: Event): void {
     const input = event.target as HTMLInputElement;
     if (!input.files?.length) return;
