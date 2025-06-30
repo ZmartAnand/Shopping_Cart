aedb6c67a0fd8aebf7312340a5884c6f0a097a06
Anand <s.anandsiva7897@gmail.com>
2025-06-30 18:05:33 +0530

    changed

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -4,10 +4,11 @@ import { Router, RouterLink } from "@angular/router";
 import { NavbarComponent } from "../navbar/navbar.component";
 import { CartService } from "../../services/cart.service";
 import { FirestoreService } from "../../../firestore.service";
+import { FooterComponent } from "../footer/footer.component";
 
 @Component({
   selector: "app-home",
-  imports: [CommonModule, RouterLink, NavbarComponent],
+  imports: [CommonModule, RouterLink, NavbarComponent, FooterComponent],
   templateUrl: "./home.component.html",
   styleUrl: "./home.component.css",
 })
