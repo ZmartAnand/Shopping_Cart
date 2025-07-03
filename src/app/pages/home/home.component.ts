8f321f003d4f25cb1aa0f12de90485670aaae29d
Anand <s.anandsiva7897@gmail.com>
2025-07-03 13:28:23 +0530

    changed

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -263,6 +263,9 @@ export class HomeComponent {
   }
 
   getStars(rating: number) {
+    if (typeof rating !== 'number' || rating < 0 || rating > 5) {
+      rating = 0;
+    }
     const fullStars = Math.floor(rating);
     const hasHalfStar = rating % 1 >= 0.5;
     const emptyStars = 5 - fullStars - (hasHalfStar ? 1 : 0);
